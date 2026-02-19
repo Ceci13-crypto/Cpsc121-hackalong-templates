529d5a5112ad204c743fd1ac326b1f83e616e1e8
Ceci13-crypto <Ceci13@csu.fullerton.edu>
2026-02-19 19:46:24 +0000

    Complete

--- The.cpp
+++ The.cpp
@@ -1,3 +1,34 @@
+//Cecilia Molina
+//832269575
+
+#include <iostream>
+#include <string>
+template <typename T, typename U> class box {
+  private:
+    T item_;
+    U other_item_;
+
+  public:
+    box(T item, U other_item):item_(item),
+      other_item_(other_item){}
+
+    void SetValue(T item, U other_item) { 
+      item_ = item;
+      other_item_ = other_item;
+     }
+
+    T GetValue() { return item_; }
+
+    void Print() {
+      std::cout << "The first item is " << item_
+                << "\nThe second item is "
+                << other_item_ << "\n";
+    }
+};
+
 int main() {
+  std::cout << "test here" << "\n";
+  box b_pair(5, 5.5);
+  b_pair.Print();
   return 0;
 }
